209871ea04f906c44f33fcc75e7f5c9101e3dc8d
jeongseongcheol <tjdcjf9333@gmail.com>
2024-03-05 02:09:39 +0900

    test

--- src/components/mainIntroduce.tsx
+++ src/components/mainIntroduce.tsx
@@ -4,7 +4,7 @@ const App: React.FC = () => {
   return (
     <div className='mainNavCss'>
       <div>안녕하세요.<br></br>제 집에 방문해 주셔서 감사합니다. </div>
-      <div>test</div>
+      <div>test 입니다.</div>
     </div>
   );
 };
